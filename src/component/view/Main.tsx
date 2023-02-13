245f9aa083dbccc47741f612f560fb6a7d6d7374
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-02-12 20:46:18 -0600

    Added React Redux

--- src/component/view/Main.tsx
+++ src/component/view/Main.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { State } from '../../redux/store';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 
-const Main = () => {
+type StateProps = {};
+
+type DispatchProps = {};
+
+type MainProps = StateProps & DispatchProps;
+
+const ConnectedMain = (props: MainProps) => {
   return (
     <div>
     </div>
   );
 };
 
+const mapStateToProps = (state: State): StateProps => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
+  return {
+
+  };
+};
+
+const Main = connect(mapStateToProps, mapDispatchToProps)(ConnectedMain);
 export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom/client';
 import Main from './component/view/Main';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
+
+const store = configureStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Main /> } />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Main /> } />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
