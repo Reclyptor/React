1ec40379962e07684e5635c7a02633834211025a
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-02-12 21:09:07 -0600

    Added TailwindCSS

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './component/view/Main';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import configureStore from './redux/store';
+import './index.css';
 
 const store = configureStore();
 
